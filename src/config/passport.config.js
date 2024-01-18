a755efb310b56dc09c31f7436cd1d2144c71ef7b
Sergio Luciano Pérez <slperez@proveedor.teco.com.ar>
2024-01-18 02:45:51 -0300

    agrego login con github

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,5 +1,6 @@
 import passport from "passport";
 import local from "passport-local";
+import GitHubStrategy from "passport-github2"
 
 import userModel from "../dao/models/user.model.js";
 import {createHash, validatePassword} from "../utils.js";
@@ -23,7 +24,6 @@ const inicializePassport = () => {
                 first_name,
                 last_name,
                 email,
-                age,
                 password: createHash(password)
             }
             const result = await userModel.create(newUser);
@@ -62,6 +62,33 @@ const inicializePassport = () => {
         let user = await userModel.findById(id);
         done(null, user)
     });
+
+    passport.use("github", new GitHubStrategy({
+        clientID: "Iv1.299729d43fce9e1b",
+        clientSecret: "35635026357a93226cd1905c5a206b52de95ecaf",
+        callbackURL: "http://localhost:8080/api/sessions/githubcallback"
+    }, async(accessToken, refreshToken, profile, done) =>{
+        try {
+            console.log(profile)
+
+            let user = await userModel.findOne({email:profile._json.email});
+            if(user){
+                console.log('Usuario ya registrado');
+                return done(null,user)
+            }
+            const newUser = {
+                first_name: profile._json.name,
+                last_name: "",
+                email: profile._json.email,
+                password: ""
+            }
+            const result = await userModel.create(newUser);
+            return done (null, result);
+
+        } catch (error) {
+            return done(error);
+        }
+    }))
 
 }
 
--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -103,6 +103,13 @@ router.get("/faillogin", (req, res) => {
     res.send({error: "fail login"});
 })
 
+router.get("/github", passport.authenticate("github", {scope: ["user:email"]}), async (req, res) => {})
+
+router.get("/githubcallback", passport.authenticate("github", {failureRedirect: "/login"}), async (req, res) => {
+    req.session.user = req.user;
+    res.redirect("/")
+})
+
 router.get('/logout', async (req, res) => {
     req.session.destroy(err=> {
         if (err) {
