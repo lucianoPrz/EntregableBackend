aba63587fc75e3910e851536ac97045ecd207ccc
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2024-02-08 00:07:47 -0300

    agrego todos los metodos de user controller

---
+++ src/controller/user.controller.js
@@ -0,0 +1,70 @@
+
+class UserController {
+    static register = async(req, res) => {
+        res.send({
+            status: 'success',
+            msg: 'usuario registrado'
+        })
+    }
+
+    static failRegister = async(req, res) => {
+        console.log("Fallo el registro");
+        res.send({ error: 'Fallo el registro' });
+    }
+
+    static login = async (req, res) => {
+        if(!req.user){
+            return res.status(400).send({status: "error"})
+        }
+        req.session.user = {
+            full_name: `${req.user.first_name} ${req.user.last_name}`,
+            email: req.user.email
+        }
+        res.send({
+            status: "success",
+            paylaod: req.user
+        })
+    }
+
+    static failLogin = (req, res) => {
+        console.log("Fallo el login");
+        res.send({ error: 'Fallo el login' });
+    }
+
+    static loginGithubCallback = async (req, res) => {
+        req.session.user = {
+            full_name: `${req.user.first_name} ${req.user.last_name}`,
+            email: req.user.email
+        }
+        res.redirect("/")
+    }
+
+    static logout = async (req, res) => {
+        req.session.destroy(err=> {
+            if (err) {
+                return res.status(500).send({
+                    status: 'error',
+                    error: 'No se pudo procesar'
+                });
+            }
+            res.redirect('/login');
+        })
+    }
+
+    static current = async (req, res) => {
+        if(!req.session.user) {
+            return res.status(400).send({status: "error"})
+        }
+        let user = {
+            full_name: req.session.user.full_name,
+            email: req.session.user.email
+        }
+    
+        res.send({
+            status: "success",
+            paylaod: user
+        })
+    }
+}
+
+export { UserController }
--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -2,143 +2,29 @@ import { Router } from 'express';
 import userModel from '../dao/models/user.model.js';
 import { createHash, validatePassword } from '../utils.js';
 import passport from 'passport';
+import { UserController } from '../controller/user.controller.js';
 
 const router = Router();
 
-// router.post('/register', async (req, res) => {
-//     const {first_name, last_name, email, password} = req.body;
-
-//     const exists = await userModel.findOne({email})
-
-//     if (exists) {
-//         return res.status(400)
-//         .send({
-//             status: 'error',
-//             error: 'El usuario ya existe'
-//         })
-//     }
-//     const user = {
-//         first_name,
-//         last_name,
-//         email,
-//         password: createHash(password)
-//     }
-
-  
-
-//     let result = await userModel.create(user)
-
-
-//     res.send({
-//         status: 'success',
-//         msg: 'usuario registrado'
-//     })
-// })
 
 router.post('/register', passport.authenticate("register", {failureRedirect:"/api/sessions/failregister"}),
-async(req, res) => {
-    res.send({
-        status: 'success',
-        msg: 'usuario registrado'
-    })
-});
+UserController.register);
 
-router.get('/failregister', async(req, res) => {
-    console.log("Fallo el registro");
-    res.send({ error: 'Fallo el registro' });
-});
+router.get('/failregister', UserController.failRegister);
 
-// router.post('/login', async (req, res) => {
-//     const {email, password} = req.body
-//     const user = await userModel.findOne({email});
-
-//     if (!user) {
-//         return res.status(400)
-//         .send({
-//             status: 'error',
-//             error: 'datos incorrectos'
-//         })
-//     }
-
-//     const isValidPassword = validatePassword(password, user)
-
-//     if(!isValidPassword){
-//         return res.status(400)
-//         .send({
-//             status: 'error',
-//             error: 'datos incorrectos'
-//         })
-//     }
-
-//     req.session.user = {
-//         full_name: `${user.first_name} ${user.last_name}`,
-//         email: user.email
-//     }
-
-//     res.send({
-//         status: 'success',
-//         payload: req.session.user,
-//         msg: 'Mi primer login'
-//     })
-
-// })
 
 router.post('/login', passport.authenticate("login", {failureRedirect: '/api/sessions/faillogin'}),
-async (req, res) => {
-    if(!req.user){
-        return res.status(400).send({status: "error"})
-    }
-    req.session.user = {
-        full_name: `${req.user.first_name} ${req.user.last_name}`,
-        email: req.user.email
-    }
-    res.send({
-        status: "success",
-        paylaod: req.user
-    })
-}
-)
+UserController.login)
 
-router.get("/faillogin", (req, res) => {
-    res.send({error: "fail login"});
-})
+router.get("/faillogin", UserController.failLogin)
 
 router.get("/github", passport.authenticate("github", {scope: ["user:email"]}), async (req, res) => {})
 
-router.get("/githubcallback", passport.authenticate("github", {failureRedirect: "/login"}), async (req, res) => {
-    req.session.user = {
-        full_name: `${req.user.first_name} ${req.user.last_name}`,
-        email: req.user.email
-    }
-    res.redirect("/")
-})
+router.get("/githubcallback", passport.authenticate("github", {failureRedirect: "/login"}), UserController.loginGithubCallback)
 
-router.get('/logout', async (req, res) => {
-    req.session.destroy(err=> {
-        if (err) {
-            return res.status(500).send({
-                status: 'error',
-                error: 'No se pudo procesar'
-            });
-        }
-        res.redirect('/login');
-    })
-})
+router.get('/logout', UserController.logout)
 
-router.get('/current', async (req, res) => {
-    if(!req.session.user) {
-        return res.status(400).send({status: "error"})
-    }
-    let user = {
-        full_name: req.session.user.full_name,
-        email: req.session.user.email
-    }
-
-    res.send({
-        status: "success",
-        paylaod: user
-    })
-})
+router.get('/current', UserController.current)
 
 
 
