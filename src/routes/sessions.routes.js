78f024779fbb9460eaa982d5faf951efb544350e
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2024-05-07 18:23:45 -0300

    creo el session controller para manejar lo que pertenece a sesiones y lo separo del user controller

--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -2,33 +2,33 @@ import { Router } from 'express';
 import userModel from '../dao/models/user.model.js';
 import { createHash, validatePassword } from '../utils.js';
 import passport from 'passport';
-import { UserController } from '../controller/user.controller.js';
+import { SessionController } from '../controller/session.controller.js';
 
 const router = Router();
 
 
 router.post('/register', passport.authenticate("register", {failureRedirect:"/api/sessions/failregister"}),
-UserController.register);
+SessionController.register);
 
-router.get('/failregister', UserController.failRegister);
+router.get('/failregister', SessionController.failRegister);
 
 
 router.post('/login', passport.authenticate("login", {failureRedirect: '/api/sessions/faillogin'}),
-UserController.login)
+SessionController.login)
 
-router.get("/faillogin", UserController.failLogin)
+router.get("/faillogin", SessionController.failLogin)
 
 router.get("/github", passport.authenticate("github", {scope: ["user:email"]}), async (req, res) => {})
 
-router.get("/githubcallback", passport.authenticate("github", {failureRedirect: "/login"}), UserController.loginGithubCallback)
+router.get("/githubcallback", passport.authenticate("github", {failureRedirect: "/login"}), SessionController.loginGithubCallback)
 
-router.get('/logout', UserController.logout)
+router.get('/logout', SessionController.logout)
 
-router.get('/current', UserController.current)
+router.get('/current', SessionController.current)
 
-router.post('/forgot-password', UserController.forgotPassword)
+router.post('/forgot-password', SessionController.forgotPassword)
 
-router.post('/reset-password', UserController.resetPassword)
+router.post('/reset-password', SessionController.resetPassword)
 
 
 
